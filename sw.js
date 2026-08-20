3aaf477e2b90a639db4f2daaecc06be5dbe3ed5e
otutorosya <bunbunbikebkb@gmail.com>
2026-08-20 19:00:52 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='spirit-real-3d-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./spot-road.jpeg','./spot-entrance.jpeg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then(hit=>{
+    if(hit)return hit;
+    return fetch(event.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return res;
+    }).catch(()=>caches.match('./index.html'));
+  }));
+});
